575afbf2196eaf740f5bc9742ccf14d9b352eae0
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-07 11:01:56 +0300

    added a table, some problems present

--- index.php
+++ index.php
@@ -15,25 +15,26 @@ if (!$conn) {
 echo "<br/>Connected successfully";
 
 
-if (isset($_POST['message'])) {
-	$sql = "INSERT INTO messages (body) VALUES ('" . $_POST['message'] . "')";
+if (isset($_POST['email']) && isset($_POST['product']) && isset($_POST['brand']) && isset($_POST['price']) && isset($_POST['weigth']) && isset($_POST['description'])) {
+	$product_sql = "INSERT INTO products (email, product, brand, price, weigth, description) VALUES ('" . $_POST['email'] . "', '" . $_POST['product'] . "', '" . $_POST['brand'] . "', '" . $_POST['price'] . "', '" . $_POST['weigth'] . "', '" . $_POST['description'] . "')";
 }
 
-if (mysqli_query($conn, $sql)) {
-	echo "New record";
+if (mysqli_query($conn, $product_sql)) {
+	echo "Product added";
 } else {
-	echo "Error" . $sql . "<br/>". mysqli_error($conn);
-}
+	echo "Error" . $product_sql . "<br/>". mysqli_error($conn);
+} 
 
-$sql = "SELECT * FROM messages";
-$result = mysqli_query($conn, $sql);
 
-$db_messages = [];
+$product_table = "SELECT * FROM products";
+$result = mysqli_query($conn, $product_table);
+
+$from_db_products = [];
 
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-       array_push($db_messages, $row);
+       array_push($from_db_products, $row);
     }
 } else {
     echo "0 results";
@@ -64,24 +65,35 @@ mysqli_close($conn);
 			<div class="col-sm-4">
 				<form action="" method="POST">
 						<div class="form-group">
-							<label for="message">Item
-							</label>
-							<textarea class="form-control" name="message" id="message" rows="1" placeholder="Describe yourself here..."> </textarea>
+							<label for="email">Email</label>
+							<textarea class="form-control" name="email" id="email" rows="1" placeholder="Describe yourself here..."> </textarea>
+							<label for="product">Product</label>
+							<textarea class="form-control" name="product" id="product" rows="1" placeholder="Describe yourself here..."> </textarea>
+							<label for="brand">Brand</label>
+							<textarea class="form-control" name="brand" id="brand" rows="1" placeholder="Describe yourself here..."> </textarea>
+							<label for="price">Price</label>
+							<textarea class="form-control" name="price" id="price" rows="1" placeholder="Describe yourself here..."> </textarea>
+							<label for="weigth">Weigth</label>
+							<textarea class="form-control" name="weigth" id="weigth" rows="1" placeholder="Describe yourself here..."> </textarea>
+							<label for="description">Description</label>
+							<textarea class="form-control" name="description" id="description" rows="1" placeholder="Describe yourself here..."> </textarea>
 						</div>
 					<button class="btn btn-success" type="submit" id="button_submit" name="button_submit">Add</button>
 				</form>
 			</div>
 			<div class="col-sm-8">
-					<h3>Record log:<br/></h3>
-					<pre>
+					<h3>Product table:<br/></h3>
 					<?php 
-
-					foreach ($db_messages as $message) {
-						echo  "<br/><div class='card'><div class='card-block'> [" . $message['id'] . "] <strong>" . $message['time'] . "</strong>: " . $message['body'] . "</div></div>";
+					echo "<table><tr><th>No.</th><th>Time</th><th>Email</th><th>Product</th><th>Brand</th><th>Price</th><th>Weigth</th><th>Description</th></tr><tr>";
+					foreach ($from_db_products as $product_to_table) {
+						echo  "<td>" . $product_to_table['id'] . "</td><td>" . $product_to_table['date'] . "</td><td>" . $product_to_table['email'] . "</td><td>" . $product_to_table['product'] . "</td><td>" . $product_to_table['brand'] . "</td><td>" . $product_to_table['price'] . "</td><td>" . $product_to_table['weigth'] . "</td><td>" . $product_to_table['description'] . "</td>";
 					}
+					echo "</tr></table>";
+
+					print_r($_POST);
+					print_r($from_db_products);
 
 					?>
-					</pre>
 			</div>
 		</div>
 	</div>	
